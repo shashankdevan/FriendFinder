c4c3a6efaec8a913b151959e236e80480350e4e0
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-04-20 12:49:36 -0400

    Added a new activity for authentication

---
+++ src/com/example/friendfinder/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.friendfinder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
