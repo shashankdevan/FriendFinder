15eb873a9aca5ad3fa4f26821f47e955be7094db
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-04-21 01:37:58 -0400

    Listener for the buttons

--- src/com/example/friendfinder/LoginActivity.java
+++ src/com/example/friendfinder/LoginActivity.java
@@ -2,10 +2,12 @@ package com.example.friendfinder;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements View.OnClickListener {
 
     private EditText editTextUsername;
     private EditText editTextPassword;
@@ -21,5 +23,19 @@ public class LoginActivity extends Activity {
         buttonSignIn = (Button) findViewById(R.id.buttonSignIn);
         buttonRegister = (Button) findViewById(R.id.buttonRegister);
 
+        buttonSignIn.setOnClickListener(this);
+        buttonRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonSignIn:
+                Toast.makeText(this, "Sign In", Toast.LENGTH_LONG);
+                break;
+            case R.id.buttonRegister:
+                Toast.makeText(this, "Register", Toast.LENGTH_LONG);
+                break;
+        }
     }
 }
