4d40c0a619d1bae05af4914dd70a08ad34e893a1
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-04-21 01:18:11 -0400

    UI for the login / registration page.

--- src/com/example/friendfinder/LoginActivity.java
+++ src/com/example/friendfinder/LoginActivity.java
@@ -2,12 +2,24 @@ package com.example.friendfinder;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
 
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private Button buttonSignIn;
+    private Button buttonRegister;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        buttonSignIn = (Button) findViewById(R.id.buttonSignIn);
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+
     }
 }
