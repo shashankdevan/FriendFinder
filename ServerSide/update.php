c708f8e270d9d76dbd4152fe4e097689da8bcdaa
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-04-26 20:26:40 -0400

    Adding function to send notification to users.

--- ServerSide/update.php
+++ ServerSide/update.php
@@ -122,4 +122,37 @@ function table_exists($tablename, $database = false) {
     return mysql_result($res, 0) == 1;
 }
 
+function send_notification($reg_ids, $message) {
+    $url = 'https://android.googleapis.com/gcm/send';
+
+    $fields = array(
+        'registration_ids' => $reg_ids,
+        'data' => $message,
+    );
+
+    $headers = array(
+        'Authorization: key=AIzaSyAJFQyyTTShpeQ4lroqa-Ur27QBcJ3UtDg',
+        'Content-Type: application/json'
+    );
+
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+
+    $result = curl_exec($ch);
+    if ($result === FALSE) {
+        die('Curl failed: ' . curl_error($ch));
+    }
+
+    curl_close($ch);
+}
+
 ?>
