1699bbb3a72bb75f94a6947273beecae3f90821d
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-04-26 21:30:17 -0400

    Open the map on registering auser.

--- src/com/example/friendfinder/RegisterActivity.java
+++ src/com/example/friendfinder/RegisterActivity.java
@@ -2,17 +2,31 @@ package com.example.friendfinder;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import com.google.android.gcm.GCMRegistrar;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
 
 import static com.example.friendfinder.Global.SENDER_ID;
 
-public class RegisterActivity extends Activity implements View.OnClickListener {
+public class RegisterActivity extends Activity implements View.OnClickListener, DataReceiver {
 
     static final String TAG = "REGISTER";
 
@@ -23,7 +37,7 @@ public class RegisterActivity extends Activity implements View.OnClickListener {
     public static String username = null;
     public static String password = null;
 
-    Context context;
+    private Context context;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,29 +59,47 @@ public class RegisterActivity extends Activity implements View.OnClickListener {
                 password = editTextPassword.getText().toString();
 
                 GCMRegistrar.checkDevice(this);
-                final String regId = GCMRegistrar.getRegistrationId(this);
+                String regId = GCMRegistrar.getRegistrationId(this);
 
                 Log.d(TAG, regId);
 
                 if (regId.equals("")) {
                     GCMRegistrar.register(this, SENDER_ID);
                 } else {
-                    AsyncTask<Void, Void, ServerResponse> registerTask = new AsyncTask<Void, Void, ServerResponse>() {
-                        @Override
-                        protected ServerResponse doInBackground(Void... params) {
-                            return BackendServer.register(context, username, password, regId);
-                        }
-
-                        @Override
-                        protected void onPostExecute(ServerResponse response) {
-                            super.onPostExecute(response);
-                            Log.d(TAG, response.getMessage());
-                        }
-                    };
-                    registerTask.execute(null, null, null);
+                    String[] params = {regId};
+                    RegisterSession mySession = new RegisterSession();
+                    mySession.delegate = (DataReceiver) context;
+                    mySession.execute(params);
                 }
                 break;
         }
     }
 
+    @Override
+    public void receive(ServerResponse response) {
+        if (response != null) {
+            if (response.getStatusCode() == 200) {
+                Intent i = new Intent(context, MapActivity.class);
+                i.putExtra("username", username);
+                startActivity(i);
+            } else {
+                Toast.makeText(context, response.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private class RegisterSession extends AsyncTask<String, Void, ServerResponse> {
+        public DataReceiver delegate;
+
+        @Override
+        protected ServerResponse doInBackground(String... params) {
+            return BackendServer.register(context, username, password, params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(ServerResponse response) {
+            super.onPostExecute(response);
+            delegate.receive(response);
+        }
+    }
 }
